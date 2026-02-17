e3b52c2b92d419e0b67f067ab12ec5e168e425fb
vaibhav9933 <vaibhavrahane36@gmail.com>
2026-02-17 12:45:46 +0530

    17/02/2026 blogs app and blog model

--- blog_main/views.py
+++ blog_main/views.py
@@ -4,9 +4,11 @@ from blogs.models import Blog
 
 def home(request):
     categories = Category.objects.all()
-    featured_posts = Blog.objects.filter(is_featured = True).order_by('updated_at')
+    featured_posts = Blog.objects.filter(is_featured = True, status = 'Published').order_by('updated_at')
+    posts = Blog.objects.filter(is_featured = False, status = 'Published')
     context = {
         'categories': categories,
         'featured_posts': featured_posts,
+        'posts': posts,
     }
     return render(request,'home.html', context)
