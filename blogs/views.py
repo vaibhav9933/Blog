54af631b9288f1d027fc523f1a94836fc1112563
vaibhav9933 <vaibhavrahane36@gmail.com>
2026-02-22 23:53:40 +0530

    22/02/2026 blogs app and blog model

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Blog, Category
 
 # Create your views here.
+
+
+def posts_by_category(request, category_id):
+    # Fetech the posts that belongs to the category with the id of category_id
+    posts = Blog.objects.filter(status='Published', category=category_id)
+
+    return HttpResponse(posts)
