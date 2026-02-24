6d7465e378c066cba9e8c8fbf15713d50a35c52d
vaibhav9933 <vaibhavrahane36@gmail.com>
2026-02-24 15:44:41 +0530

    24/02/2026 Posts by Category

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,5 @@
+from unicodedata import category
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Blog, Category
@@ -8,5 +10,9 @@ from .models import Blog, Category
 def posts_by_category(request, category_id):
     # Fetech the posts that belongs to the category with the id of category_id
     posts = Blog.objects.filter(status='Published', category=category_id)
-
-    return HttpResponse(posts)
+    category = Category.objects.get(pk=category_id)
+    context ={
+        'posts':posts,
+        'category':category,
+    }
+    return render(request,'posts_by_category.html', context)
