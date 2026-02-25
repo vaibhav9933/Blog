2d3626deb2d87d5656f940e60ae3bc6090d4a5b4
vaibhav9933 <vaibhavrahane36@gmail.com>
2026-02-25 14:53:31 +0530

    25/02/2026 posts by category

--- blog_main/views.py
+++ blog_main/views.py
@@ -3,11 +3,11 @@ from blogs.models import Category
 from blogs.models import Blog
 
 def home(request):
-    categories = Category.objects.all()
+    # categories = Category.objects.all()
     featured_posts = Blog.objects.filter(is_featured = True, status = 'Published').order_by('updated_at')
     posts = Blog.objects.filter(is_featured = False, status = 'Published')
     context = {
-        'categories': categories,
+        # 'categories': categories,
         'featured_posts': featured_posts,
         'posts': posts,
     }
