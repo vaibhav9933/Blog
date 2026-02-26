9a30abbc6ef9836fc5c3ba91810c878fce192f58
vaibhav9933 <vaibhavrahane36@gmail.com>
2026-02-26 12:17:55 +0530

    26/02/2026 Single blog page done ....

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -4,9 +4,11 @@ from .import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include 
+from blogs import views as BlogViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
     path('category/',include('blogs.urls')),
+    path('<slug:slug>/',BlogViews.blogs, name="blogs"),
 ] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blogs/views.py
+++ blogs/views.py
@@ -21,3 +21,10 @@ def posts_by_category(request, category_id):
         'category':category,
     }
     return render(request,'posts_by_category.html', context)
+
+def blogs(request,slug):
+    single_blog = get_object_or_404(Blog, slug=slug, status='Published')
+    context ={
+        'single_blog':single_blog,
+    }
+    return render(request,'blogs.html', context)
