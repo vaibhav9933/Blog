70568edb5b17ee4ba147073d9055af11e208cd2c
vaibhav9933 <vaibhavrahane36@gmail.com>
2026-03-03 22:39:58 +0530

    Registration & Authentication done ....

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
     path('category/',include('blogs.urls')),
-    path('<slug:slug>/',BlogViews.blogs, name="blogs"),
     # Search endpoint
     path('blogs/search/', BlogViews.search, name='search'),
+    path('blogs/<slug:slug>/',BlogViews.blogs, name="blogs"),
+    path('register/', views.register, name='register'),
 ] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog_main/views.py
+++ blog_main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from blog_main.forms import RegistrationForm
 from blogs.models import Category
 from blogs.models import Blog
 from about_us.models import About
+
+
 
 def home(request):
     # categories = Category.objects.all()
@@ -21,3 +24,22 @@ def home(request):
         'about': about,
     }
     return render(request,'home.html', context)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # You can also log the user in immediately after registration if desired
+            # from django.contrib.auth import login
+            # login(request, user)
+            return render(request, 'register.html', {'form': RegistrationForm(), 'success': True})
+        else:
+            print("Form is not valid")
+    else:
+        form = RegistrationForm()
+    context={
+        'form': form,
+    }
+    return render(request,'register.html', context)
