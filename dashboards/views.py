93fa004ce8248162f351c260b412bf1ae9481b63
vaibhav9933 <vaibhavrahane36@gmail.com>
2026-03-09 17:23:43 +0530

    09/03/2026 Category CURD Functionality Done ....

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,5 +1,74 @@
-from django.shortcuts import render
+from unicodedata import category
+
+from django.shortcuts import get_object_or_404, redirect, render
+from blogs.models import Blog, Category
+from django.contrib.auth.decorators import login_required
+
+from .forms import CategoryForm
+
 
 # Create your views here.
+@login_required(login_url="login")
 def dashboard(request):
-    return render(request, 'dashboard/dashboard.html')
+    category_count = Category.objects.all().count()
+    blogs_count = Blog.objects.all().count()
+
+    context={
+        'category_count': category_count,
+        'blogs_count': blogs_count,
+    }
+    
+    return render(request, 'dashboard/dashboard.html',context)
+
+
+@login_required(login_url="login")
+def categories(request):
+    category_count = Category.objects.all().count()
+    blogs_count = Blog.objects.all().count()
+
+    context={
+        'category_count': category_count,
+        'blogs_count': blogs_count,
+    }
+    
+    return render(request, 'dashboard/categories.html', context)
+
+
+def add_category(request):
+    if request.method == "POST":
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+    form = CategoryForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'dashboard/add_category.html', context)
+
+def edit_category(request,pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == "POST":
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+    form = CategoryForm(instance=category)
+    context ={
+        'form': form,
+        'category': category,
+    }   
+    return render(request, 'dashboard/edit_category.html', context)
+
+def delete_category(request,pk):
+    category = get_object_or_404(Category,pk=pk)
+    category.delete()
+    return redirect('categories')
+
+@login_required(login_url="login")
+def posts(request):
+    blogs = Blog.objects.all()
+    context = {
+        'blogs': blogs,
+    }
+    return render(request, 'dashboard/posts.html', context)
