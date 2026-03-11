f07f09d5f607ffa57226991a5bbfacdc3f3ea1d4
vaibhav9933 <vaibhavrahane36@gmail.com>
2026-03-11 16:49:50 +0530

    12/03/2026 Comment Section Done ....

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Category
-from .models import Blog
+from .models import Blog , Comment
 
 
 class BlogAdmin(admin.ModelAdmin):
@@ -12,3 +12,4 @@ class BlogAdmin(admin.ModelAdmin):
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Blog, BlogAdmin)
+admin.site.register(Comment)
